092806b735bf2a776a8352d677213328bd71d2a6
YuriyTroian <yuriy.tr07@gmail.com>
2024-09-14 00:01:41 +0300

    adding lab1

---
+++ tabl1.py
@@ -0,0 +1,27 @@
+#Змінні
+drink="Pepsi"
+print(drink)
+
+#Оператори порівняння
+is_holiday = True
+is_workday = False
+print(is_holiday == is_workday)
+print(is_holiday != is_workday)
+print(is_holiday < is_workday)
+print(is_holiday > is_workday)
+print(is_holiday <= is_workday)
+print(is_holiday >= is_workday)
+
+#Булеві оператори
+is_sunny = True
+is_cloudy = True
+print(is_sunny or is_cloudy)
+
+#вираз
+x = 2.34
+y = 1.45
+import math
+example = (math.exp(-y))+((math.sin(x))**2)/(math.cos(y**2))-(12.1*math.log(x+y))
+print(example)
+
+
