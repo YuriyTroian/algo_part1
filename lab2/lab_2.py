6f470069badcc9918ba0439f62eb756bb0a9bafa
YuriyTroian <yuriy.tr07@gmail.com>
2024-10-12 20:30:22 +0300

    Last version of lab2

--- lab2/lab_2.py
+++ lab2/lab_2.py
@@ -4,26 +4,25 @@ x=1
 while x<=10:
     if x<3:
         func1=m.sqrt(x**4+m.log(x))
-        print("Функція №1:", func1)
+        print(f"Функція №1:{func1}")
     elif 3<=x<6:
         func2=abs(m.log(x))**5
-        print("Функція №2:", func2)
+        print(f"Функція №2:{func2}")
     elif x>=6:
         func3=(m.cos(x))**x
-        print("Функція №3:", func3)
+        print(f"Функція №3:{func3}")
     x+=0.15
 
 
 #tabl2
-y=-0.05
+y=0
 
 while y<=1:
-    y += 0.05
     sum_value = 0
     k = 1
     elem = (k*(y)**2)*m.sin(k*y)
     sum_value += elem
-    print("Функція k =", k, round(y,2), elem)
+    print(f"Функція k = {k}, y = {round(y, 2)}, elem = {elem}")
 
 
     while elem>10**(-5):
@@ -31,8 +30,14 @@ while y<=1:
         elem = (k*(y)**2)*m.sin(k*y)
         sum_value += elem
 
-        print("Функція k =", k, round(y, 2), elem)
-        print("Сума к =", k, round(y,2), y + sum_value)
+        print(f"Функція k = {k}, y = {y}, elem = {elem}")
+        print(f"Сума к = {k}, y = {y}, Sum = {y + sum_value}")
+    y += 0.05
+    y = round(y, 2)
+
+
+
+
 
 
 
