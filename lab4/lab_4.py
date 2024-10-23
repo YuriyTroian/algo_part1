d1ab97673d1acf4bf7ed306a4848b01d559b1164
YuriyTroian <yuriy.tr07@gmail.com>
2024-10-23 23:04:25 +0300

    initial commit

--- lab4/lab_4.py
+++ lab4/lab_4.py
@@ -1,3 +1,6 @@
+from cmd import PROMPT
+
+
 class Student():
     def __init__(self, name = "", rating = 0, growth = 0, course_num = 0, specialty = ""):
         self.__name = name
@@ -17,7 +20,10 @@ class Student():
     def set_name(self, name):
         self.__name = name
     def set_rating(self, rating):
-        self.__rating = rating
+        if 0 < rating < 10:
+            self.__rating = rating
+        else:
+            print("Рейтинг відємний")
     def set_growth(self, growth):
         self.__growth = growth
 
@@ -30,15 +36,25 @@ class Student():
     def __del__(self):
         print(f"Student {self.__name} was deleted")
 
-def main():
-    student1 = Student("Yurii Troian", 14, 172, 1, "IoT")
-    student2 = Student("John Joy", 30, 180, 2, "KI")
-    student3 = Student("Julie Juli", 56, 165, 3, "SA")
+def create_student(name, rating, growth, course_num, specialty):
+    if rating > 50:
+        print(f"Рейтинг {rating} завеликий. Відрахувати студента {name}.")
+        return None
 
-    print(student1)
-    print(student2)
-    print(student3)
+    return Student(name, rating, growth, course_num, specialty)
+
+def main():
+    student_Arr = [
+        create_student("Yurii Troian", 14, 172, 1, "IoT"),
+        create_student("John Joy", 30, 180, 2, "KI"),
+        create_student("Julie Juli", 56, 165, 3, "SA")
+    ]
+
+    print(student_Arr)
+
+    #student1 = create_student("Yurii Troian", 14, 172, 1, "IoT")
+    #student2 = create_student("John Joy", 30, 180, 2, "KI")
+    #student3 = create_student("Julie Juli", 56, 165, 3, "SA")
 
 main()
-
 
